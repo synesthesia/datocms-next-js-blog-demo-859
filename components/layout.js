5937bf54a87f73697cfd77335c12544c68a727be
Julian Elve <julian.elve@gmail.com>
2021-05-27 09:44:08 +0100

    feat: add navbar

--- components/layout.js
+++ components/layout.js
@@ -1,11 +1,13 @@
 import Alert from '../components/alert'
 import Footer from '../components/footer'
+import Navbar from '../components/navbar'
 
 export default function Layout({ preview, children }) {
   return (
     <>
       <div className="min-h-screen">
         <Alert preview={preview} />
+        <Navbar />
         <main>{children}</main>
       </div>
       <Footer />
--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import '../styles/index.css'
+import "@fortawesome/fontawesome-free/css/all.min.css";
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
